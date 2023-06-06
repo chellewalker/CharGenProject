d1051e9815d468213a4dd6e8979b5ce90a707623
chellewalker <war19006@byui.edu>
2023-06-06 17:40:06 -0400

    finished starshipManeuvers functions

--- javascripts/feats/getStarshipManeuver.js
+++ javascripts/feats/getStarshipManeuver.js
@@ -1,4 +1,4 @@
-export function getStarshipManeuver(starshipManeuvers,available,wis) {
+export function getStarshipManeuver(starshipManeuvers,wis) {
     let temp;
     let count;
     let avail = 1 + Math.max(0,Math.floor((wis-10)/2));
@@ -6,12 +6,87 @@ export function getStarshipManeuver(starshipManeuvers,available,wis) {
     for (count = 0; count < avail; count++) {
         temp = "";
     while (temp == "") {
-        let randomNum = Math.floor(Math.random() * 26);
+        let randomNum = Math.floor(Math.random() * 27);
         if (randomNum == 0) {
             temp = "Ackbar Slash";
         }
-        if (randomNum == 1) {
+        else if (randomNum == 1) {
             temp = "Afterburn";
+        }
+        else if (randomNum == 2) {
+            temp = "Angle Deflector Shields";
+        }
+        else if (randomNum == 3) {
+            temp = "Attack Formation Zeta Nine";
+        }
+        else if (randomNum == 4) {
+            temp = "Attack Pattern Delta";
+        }
+        else if (randomNum == 5) {
+            temp = "Corellian Slip";
+        }
+        else if (randomNum == 6) {
+            temp = "Counter";
+        }
+        else if (randomNum == 7) {
+            temp = "Darklighter Spin";
+        }
+        else if (randomNum == 8) {
+            temp = "Devastating Hit";
+        }
+        else if (randomNum == 9) {
+            temp = "Engine Hit";
+        }
+        else if (randomNum == 10) {
+            temp = "Evasive Action";
+        }
+        else if (randomNum == 11) {
+            temp = "Explosive Shot";
+        }
+        else if (randomNum == 12) {
+            temp = "Howlrunner Formation";
+        }
+        else if (randomNum == 13) {
+            temp = "I Have You Now";
+        }
+        else if (randomNum == 14) {
+            temp = "Intercept";
+        }
+        else if (randomNum == 15) {
+            temp = "Overwhelming Assault";
+        }
+        else if (randomNum == 16) {
+            temp = "Segnor's Loop";
+        }
+        else if (randomNum == 17) {
+            temp = "Shield Hit";
+        }
+        else if (randomNum == 18) {
+            temp = "Skim the Surface";
+        }
+        else if (randomNum == 19) {
+            temp = "Skywalker Loop";
+        }
+        else if (randomNum == 20) {
+            temp = "Snap Roll";
+        }
+        else if (randomNum == 21) {
+            temp = "Strike Formation";
+        }
+        else if (randomNum == 22) {
+            temp = "Tallon Roll";
+        }
+        else if (randomNum == 23) {
+            temp = "Target Lock";
+        }
+        else if (randomNum == 24) {
+            temp = "Target Sense";
+        }
+        else if (randomNum == 25) {
+            temp = "Thruster Hit";
+        }
+        else if (randomNum == 26) {
+            temp = "Wotan Weave";
         }
         if (count == 25) {
             temp = "ValidForcePowerNotFound";
